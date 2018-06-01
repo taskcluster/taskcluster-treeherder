ae3cb9fc11b94e958f720ad64841816df0db5bb6
Tom Prince <mozilla@hocat.ca>
2018-06-01 17:19:36 -0600

    Bug 1400258: Add support for generating `chain-of-trust` logs for treeherder.

--- src/transform/artifact_links.js
+++ src/transform/artifact_links.js
@@ -1,6 +1,12 @@
 const path = require('path');
 const taskcluster = require('taskcluster-client');
 
+LOG_ARTIFACTS = {
+    // XXX: This is a magical name see 1147958 which enables the log viewer.
+    'public/logs/live_backing.log': 'builds-4h',
+    'public/logs/chain_of_trust.log': 'chain-of-trust',
+}
+
 module.exports = async function(queue, monitor, taskId, runId, job) {
   let res;
   try {
@@ -28,6 +34,14 @@ module.exports = async function(queue, monitor, taskId, runId, job) {
   let seen = {};
 
   let links = artifacts.map((artifact) => {
+    let url = `https://queue.taskcluster.net/v1/task/${taskId}` +
+	       `/runs/${runId}/artifacts/${artifact.name}`;
+    if (artifact.name in LOG_ARTIFACTS) {
+	job.logs.push({
+	    name: LOG_ARTIFACTS[artifact.name],
+	    url: url,
+	});
+    }
     let name = path.parse(artifact.name).base;
     if (!seen[name]) {
       seen[name] = [artifact.name];
@@ -38,8 +52,7 @@ module.exports = async function(queue, monitor, taskId, runId, job) {
     let link = {
       label: 'artifact uploaded',
       linkText: name,
-      url: `https://queue.taskcluster.net/v1/task/${taskId}` +
-             `/runs/${runId}/artifacts/${artifact.name}`,
+      url: url,
     };
     return link;
   });
